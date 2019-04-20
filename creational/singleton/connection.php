c13d3475df694248fb0a9cff1aebc47e8fe8aeba
Jeremy Reyes B <jjrb6@hotmail.com>
2019-04-20 13:54:18 -0500

    [feat]: Created example singleton pattern.

---
+++ creational/singleton/connection.php
@@ -0,0 +1,22 @@
+<?php
+
+class Connection
+{
+    private function __construct()
+    {
+        echo 'Hemos creado un nuevo objeto <br />';
+    }
+
+    public static function getInstance()
+    {
+        static $instance = null;
+
+        if (null === $instance) {
+            $instance = new static();
+        } else {
+            echo 'El objeto ya existe, no puedes volver a crearlo <br />';
+        }
+
+        return $instance;
+    }
+}
--- creational/singleton/index.php
+++ creational/singleton/index.php
@@ -1,1 +1,8 @@
 <?php
+
+
+include 'connection.php';
+
+$object  = Connection::getInstance();
+$object2 = Connection::getInstance();
+$object3 = Connection::getInstance();
